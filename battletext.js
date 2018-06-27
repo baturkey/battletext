ea8eba10d67bd0800a0227f333ee9f8894b91d4a
Jeffrey Liu <jeff.liu@baublebar.com>
2018-06-27 13:35:34 -0400

    work on range object

--- battletext.js
+++ battletext.js
@@ -1,4 +1,19 @@
-let range = 10;
 $(document).ready(function(){
-    $("#doit").click(() => console.log('a'));
+    'use strict';
+    let range = {
+        distance: 0,
+        delta: function(diff) {
+            return () => {
+                this.distance += diff;
+                $("#range").html(this.distance);
+                $("#enemy").css("fontSize", (-this.distance / 4 + 5) + "rem");
+            };
+        },
+    };
+
+    range.delta(10)();
+
+    $("#doit").click(range.delta(3));
+    $("#near").click(range.delta(-1));
+    $("#far").click(range.delta(1));
 });
