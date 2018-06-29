24bdd9516c865302d379a66c11cc31546c590a16
Jeffrey Liu <jeff.liu@baublebar.com>
2018-06-29 16:16:34 -0400

    Work on UI

--- battletext.js
+++ battletext.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
     'use strict';
+
+    $('[data-toggle="tooltip"]').tooltip();
+
     const range = {
         distance: 0,
         delta: function(diff) {
-            return () => {
-                if (-diff > this.distance) {
-                    $("#enemy").css("transition", "2s").css("transform", "rotateY(180deg)");
-                }
-                this.distance = Math.abs(this.distance + diff);
-                $("#range").html(this.distance);
-                $("#enemy").css("fontSize", (-this.distance / 4 + 5) + "rem");
-            };
+            if (-diff > this.distance) {
+                $("#enemy").css("transition", "2s").css("transform", "rotateY(180deg)");
+            }
+            this.distance = Math.abs(this.distance + diff);
+            $("#range").html(this.distance);
+            $("#enemy").css("fontSize", (-this.distance / 4 + 5) + "rem");
         },
     };
 
@@ -18,22 +19,34 @@ $(document).ready(function(){
         energy: 0,
         max: 100,
         delta: function(diff) {
-            return () => {
-                this.energy += diff;
-                $("#battery")
-                    .css("width", (this.energy * 100 / this.max) + "%")
-                    .html(this.energy)
-                    .attr("aria-valuenow", this.energy);
-            }
+            this.energy += diff;
+            $("#battery")
+                .css("width", (this.energy * 100 / this.max) + "%")
+                .html(this.energy)
+                .attr("aria-valuenow", this.energy);
         },
     };
 
-    range.delta(10)();
-    battery.delta(10)();
+    let weaponSettings = {};
 
-    $("#near").click(range.delta(-1));
-    $("#far").click(range.delta(1));
+    $("input[type=radio]:checked").each(function() {
+        weaponSettings[$(this).attr("name")] = $(this).data("energy");
+    });
 
-    $("#add").click(battery.delta(1));
-    $("#sub").click(battery.delta(-1));
+    $("input[type=radio]").change(function(e) {
+        console.log($(this).data("energy"), $(this).attr("name"), weaponSettings);
+        battery.delta(weaponSettings[$(this).attr("name")] - $(this).data("energy"));
+        weaponSettings[$(this).attr("name")] = $(this).data("energy");
+    });
+
+    range.delta(10);
+    battery.delta(10);
+
+    $("#doit").click(function() {
+        $("input[type=radio]:checked").each(function() {
+            console.log(this);
+        });
+
+        range.delta(Math.floor(Math.random() * 4) - 3);
+    });
 });
