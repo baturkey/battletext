fcd962424580af826de66bc84d16ae01372521db
Jeffrey Liu <jeff.liu@baublebar.com>
2018-07-02 23:38:49 -0400

    Make weapons more dynamic

--- battletext.js
+++ battletext.js
@@ -31,33 +31,77 @@ $(document).ready(function(){
 
     let weapons = [
         {
+            id: 0,
             name: 'Omegaton Missile',
             opts: [
                 {
+                    id: 1,
                     range: 4,
                     energy: 1,
                 },
                 {
+                    id: 2,
                     range: 5,
                     energy: 2,
                 },
             ],
+            dmg: 0,
         },
         {
+            id: 1,
             name: 'Rivetripper Sword',
             opts: [
                 {
+                    id: 1,
                     range: 0,
                     energy: 1,
                 },
                 {
+                    id: 2,
                     range: 1,
                     energy: 2,
                 },
             ],
+            dmg: 1,
+        },
+        {
+            id: 2,
+            name: 'Jigglywatt Laser',
+            opts: [
+                {
+                    id: 1,
+                    range: 3,
+                    energy: 2,
+                },
+                {
+                    id: 2,
+                    range: 4,
+                    energy: 3,
+                },
+            ],
+            dmg: 2,
         },
     ];
 
+    function populateWeaponList(w) {
+        let list = "";
+
+        const dmgMap = ['success', 'warning', 'danger'];
+
+        w.forEach(function(x) {
+            list += "<li class='list-group-item d-flex'><h3 class='flex-fill text-" + dmgMap[x.dmg] + "'>" + x.name + "</h3><div class='btn-group btn-group-toggle flex-fill' data-toggle='buttons'>";
+            list += "<label class='btn btn-secondary active'><input type='radio' name='weapon_" + x.id + "' id='weapon'" + x.id + "_0' data-energy='0'> Do not fire</label>";
+            x.opts.forEach(function(opt) {
+                list += "<label class='btn btn-secondary'><input type='radio' name='weapon_" + x.id + "' id='weapon'" + x.id + "_" + opt.id + "' data-energy='" + opt.energy + "'> " + opt.range + "</label>";
+            });
+            list += "</div></li>";
+        });
+
+        $("#weaponList").html(list);
+    }
+
+    populateWeaponList(weapons);
+    
     $("input[type=radio]:checked").each(function() {
         weaponSettings[$(this).attr("name")] = $(this).data("energy");
     });
