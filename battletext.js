03494f09bf1b184deb6f40ab57966679f4f0cdfc
Jeffrey Liu <jeff.liu@baublebar.com>
2018-07-05 13:48:58 -0400

    Make more data driven

--- battletext.js
+++ battletext.js
@@ -25,17 +25,25 @@ $(document).ready(function(){
                 .html(this.energy)
                 .attr("aria-valuenow", this.energy);
 
-            $("input[type=radio]").each(function() {
+            $("input[type=radio]").each((_, e) => {
+                if($(e).data('energy') > this.energy) {
+                    $(e).attr('disabled', true);
+                    $(e).parent().addClass('text-muted');
+                } else {
+                    $(e).attr('disabled', false);
+                    $(e).parent().removeClass('text-muted');
+                }
             });
         },
     };
 
     let weaponSettings = {};
 
-    let playerWeapons = [
+    let player = [
         {
             id: 0,
             name: 'Omegaton Missile',
+            type: 'weapons',
             opts: [
                 {
                     id: 1,
@@ -53,6 +61,7 @@ $(document).ready(function(){
         {
             id: 1,
             name: 'Rivetripper Sword',
+            type: 'weapons',
             opts: [
                 {
                     id: 1,
@@ -70,6 +79,7 @@ $(document).ready(function(){
         {
             id: 2,
             name: 'Jigglywatt Laser',
+            type: 'weapons',
             opts: [
                 {
                     id: 1,
@@ -84,33 +94,70 @@ $(document).ready(function(){
             ],
             dmg: 2,
         },
+        {
+            id: 3,
+            name: 'Walking',
+            type: 'movement',
+            opts: [
+                {
+                    id: 1,
+                    range: -1,
+                    energy: 0,
+                },
+                {
+                    id: 2,
+                    range: 1,
+                    energy: 0,
+                },
+                {
+                    id: 3,
+                    range: 2,
+                    energy: 2,
+                },
+            ]
+        },
+        {
+            id: 4,
+            name: 'Chaff',
+            type: 'equipment',
+            opts: [
+                {
+                    id: 1,
+                    range: 0,
+                    energy: 0,
+                }
+            ]
+        },
     ];
 
-    function populateWeaponList(w) {
-        let list = "";
+    const types = player.reduce((acc, cur) => {
+        if (!acc.includes(cur.type)) {
+            acc.push(cur.type);
+        }
+        return acc;
+    }, []);
 
-        const dmgMap = ['success', 'warning', 'danger'];
-
-        w.forEach(function(x) {
-            list += "<li class='list-group-item d-flex'><h3 class='flex-fill text-" + dmgMap[x.dmg] + "'>" + x.name + "</h3><div class='btn-group btn-group-toggle flex-fill' data-toggle='buttons'>";
-            list += "<label class='btn btn-secondary active'><input type='radio' name='weapon_" + x.id + "' id='weapon'" + x.id + "_0' data-energy='0'> Do not fire</label>";
-            x.opts.forEach(function(opt) {
-                list += "<label class='btn btn-secondary'><input type='radio' name='weapon_" + x.id + "' id='weapon'" + x.id + "_" + opt.id + "' data-energy='" + opt.energy + "'> " + opt.range + "</label>";
-            });
-            list += "</div></li>";
-        });
-
-        $("#weaponList").html(list);
+    for (let type of types) {
+        $("#loadout").append("<h2>" + type[0].toUpperCase() + type.substr(1) + "</h2><ul id='" + type + "List' class='list-group'></ul>");
     }
 
-    populateWeaponList(playerWeapons);
+    const dmgMap = ['success', 'warning', 'danger'];
+
+    for (let x of player) {
+        let list = "<li class='list-group-item d-flex'><h3 class='flex-fill text-" + dmgMap[x.dmg] + "'>" + x.name + "</h3><div class='btn-group btn-group-toggle flex-fill' data-toggle='buttons'>";
+        list += "<label class='btn btn-secondary active'><input type='radio' name='weapon_" + x.id + "' id='weapon'" + x.id + "_0' data-energy='0' checked> Hold</label>";
+        for (let opt of x.opts) {
+            list += "<label class='btn btn-secondary'><input type='radio' name='weapon_" + x.id + "' id='weapon'" + x.id + "_" + opt.id + "' data-energy='" + opt.energy + "'> " + opt.range + "</label>";
+        }
+        list += "</div></li>";
+        $("#" + x.type + "List").append(list);
+    }
 
     $("input[type=radio]:checked").each(function() {
         weaponSettings[$(this).attr("name")] = $(this).data("energy");
     });
 
     $("input[type=radio]").change(function(e) {
-        console.log($(this).data("energy"), $(this).attr("name"), weaponSettings);
         battery.delta(weaponSettings[$(this).attr("name")] - $(this).data("energy"));
         weaponSettings[$(this).attr("name")] = $(this).data("energy");
     });
